d337adb0993f0a155825c687ff9c4307814333a6
amulenkan <115099530+amulenkan@users.noreply.github.com>
2023-04-10 08:50:10 +0300

    sec config

--- PP_3_1_5_Rest/src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ PP_3_1_5_Rest/src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -29,7 +29,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user").hasAnyRole("ADMIN", "USER")
-                .antMatchers(("/api/**")).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
